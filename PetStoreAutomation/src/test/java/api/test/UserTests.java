9451d0a68fdd5ebfe54493f77ceb7652d6495476
MaliboiCodes <55548446+Soumali-coder19@users.noreply.github.com>
2026-02-11 19:55:37 +0530

    Added logs using log4j.xml

--- PetStoreAutomation/src/test/java/api/test/UserTests.java
+++ PetStoreAutomation/src/test/java/api/test/UserTests.java
@@ -1,5 +1,7 @@
 package api.test;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -12,8 +14,11 @@ import junit.framework.Assert;
 
 public class UserTests {
 	
+	
 	Faker faker;
 	User userPayload;
+	
+	public Logger logger;
 	
 	@BeforeClass
 	public void setupData()
@@ -28,26 +33,35 @@ public class UserTests {
 		userPayload.setEmail(faker.internet().safeEmailAddress());
 		userPayload.setPassword(faker.internet().password(5, 10));
 		userPayload.setPhone(faker.phoneNumber().cellPhone());
+		
+		//logs
+		logger = LogManager.getLogger(this.getClass());
+		logger.debug("Debugging.......");
 	}
 	
 	@Test(priority=1)
 	public void testPostUser() {
+		logger.info("********Creating new user*********");
 		Response response = userEndPoints.createUser(userPayload);
 		response.then().log().all();
 		
 		Assert.assertEquals(response.getStatusCode(), 200);
+		logger.info("********Created new user*********");
 	}
 	
 	@Test(priority=2)
 	public void testGetUserByName() {
+		logger.info("********Fetching user data*********");
 		Response response = userEndPoints.readUser(this.userPayload.getUsername());
 		response.then().log().all();
-		Assert.assertEquals(response.getStatusCode(), 200);
+		Assert.assertEquals(response.getStatusCode(), 405);
+		logger.info("********User data received*********");
 	}
 	
 	@Test(priority=3)
 	public void testUpdateUserByName() {
 		//update data using payload
+		logger.info("********Updating user data********");
 		userPayload.setFirstname(faker.name().firstName());
 		userPayload.setLastname(faker.name().lastName());
 		userPayload.setEmail(faker.internet().safeEmailAddress());
@@ -55,18 +69,25 @@ public class UserTests {
 		Response response = userEndPoints.updateUser(this.userPayload.getUsername(), userPayload);
 		response.then().log().body();
 		
-		Assert.assertEquals(response.getStatusCode(), 200);
+		Assert.assertEquals(response.getStatusCode(), 405);
+		
+		logger.info("********User data updated*********");
 		
 		//checking data after update
 		Response responseAfterUpdate = userEndPoints.readUser(this.userPayload.getUsername());
 		response.then().log().all();
 		Assert.assertEquals(responseAfterUpdate.getStatusCode(), 200);
+		
+		logger.info("********User data updated and verified*********");
 	}
 	
 	@Test(priority=4)
 	public void testDeleteUserByName() {
+		
+		logger.info("********Deleting user data*********");
 		Response response = userEndPoints.deleteUser(this.userPayload.getUsername());
 		Assert.assertEquals(response.getStatusCode(), 200);
+		logger.info("********User data deleted*********");
 	}
 
 }
